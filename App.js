2db5dfcbc8e50b0b06da5593b812e33c89e98bf3
Ray Arvin Rimorin <rayarvin@gmail.com>
2018-04-06 06:50:03 +0800

    wraparound picker

--- App.js
+++ App.js
@@ -3,11 +3,32 @@ import { StyleSheet, View, Text } from 'react-native';
 import Tripmeter from './lib/Tripmeter';
 
 export default class App extends React.Component {
+
+  static defaultProps = {
+    initialValue: '789456',
+    tripmeterHeight: 150,
+    boxHeight: 50,
+  }
+
+  state = {
+    tripMeterValue: 0
+  }
+
+  onTripMeterChange = (value) => {
+    this.setState({ tripMeterValue: value });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{ marginTop: 55 }}>Tripmeter test</Text>
-        <Tripmeter initial='750392' tripmeterHeight={150} boxHeight={50} />
+        <Text style={{ marginTop: 55 }}>Tripmeter initial value: {this.props.initialValue}</Text>
+        <Tripmeter 
+          initial={this.props.initialValue}
+          tripmeterHeight={this.props.tripmeterHeight}
+          boxHeight={this.props.boxHeight}
+          onTripMeterChange={this.onTripMeterChange}
+        />
+        <Text>Selected value: {this.state.tripMeterValue}</Text>
       </View>
     );
   }
