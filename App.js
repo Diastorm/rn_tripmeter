b0f02c43984d61b6a972f87fc4481d18a89a3bb3
Ray Arvin Rimorin <rayarvin@gmail.com>
2018-04-06 10:42:28 +0800

    change initial to int

--- App.js
+++ App.js
@@ -4,40 +4,42 @@ import { Tripmeter } from './lib/Tripmeter';
 
 export default class App extends React.Component {
 
-  static defaultProps = {
-    initialValue: '789456',
-    tripmeterHeight: 150,
-    boxHeight: 50,
-  }
+    static defaultProps = {
+        initialValue: 789,
+        tripmeterHeight: 150,
+        boxHeight: 50,
+        digits: 7
+    }
 
-  state = {
-    tripMeterValue: 0
-  }
+    state = {
+        tripMeterValue: 0
+    }
 
-  onTripMeterChange = (value) => {
-    this.setState({ tripMeterValue: value });
-  }
+    onTripMeterChange = (value) => {
+        this.setState({ tripMeterValue: value });
+    }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={{ marginTop: 55 }}>Tripmeter initial value: {this.props.initialValue}</Text>
-        <Tripmeter 
-          initial={this.props.initialValue}
-          tripmeterHeight={this.props.tripmeterHeight}
-          boxHeight={this.props.boxHeight}
-          onTripMeterChange={this.onTripMeterChange}
-        />
-        <Text>Selected value: {this.state.tripMeterValue}</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={{ marginTop: 55 }}>Tripmeter initial value: {this.props.initialValue}</Text>
+                <Tripmeter 
+                    digits={this.props.digits}
+                    initial={this.props.initialValue}
+                    tripmeterHeight={this.props.tripmeterHeight}
+                    boxHeight={this.props.boxHeight}
+                    onTripMeterChange={this.onTripMeterChange}
+                />
+                <Text>Selected value: {this.state.tripMeterValue}</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#2089dc',
-    flex: 1,
-    justifyContent: 'center',
-  },
+    container: {
+        backgroundColor: '#2089dc',
+        flex: 1,
+        justifyContent: 'center',
+    },
 });
